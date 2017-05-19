16caa68f166b790f8e701bf84fd45d7afb314bd8
刘伟强 <liuweiqiang@jianke.com>
2017-05-19 16:14:43 +0800

    创建头文件

---
+++ JKNetWorking/JKNetWorking.h
@@ -0,0 +1,17 @@
+//
+//  JKNetWorking.h
+//  test
+//
+//  Created by 刘伟强 on 2017/5/19.
+//  Copyright © 2017年 刘伟强. All rights reserved.
+//
+
+#ifndef JKNetWorking_h
+#define JKNetWorking_h
+
+#import "BGNetworkManager.h"
+#import "BGAFRequestSerializer.h"
+#import "BGAFResponseSerializer.h"
+#import "BGBatchRequest.h"
+
+#endif /* JKNetWorking_h */
